5669f2827fa3746810ba1cf57527d5c222ac4feb
squilliam12 <wfan@wm.edu>
2026-06-24 17:14:22 -0400

    Implement greeks

--- src/option.py
+++ src/option.py
@@ -30,4 +30,35 @@ class Option:
         elif self.option_type == 'put':
             return (np.exp(-self.r*self.T)*self.K*norm.cdf(-self.d2)
             - self.S*norm.cdf(-self.d1))
+
+    def delta(self):
+        """Calculate delta, the change in an option's price w.r.t. the underlying"""
+        if self.option_type == 'call':
+            return norm.cdf(self.d1)
+        elif self.option_type == 'put':
+            return norm.cdf(self.d1)-1
         
+    def gamma(self):
+        """Calculate gamma, the change in an option's delta w.r.t. the underlying"""
+        return norm.pdf(self.d1)/(self.S*self.sigma*np.sqrt(self.T))
+
+    def vega(self):
+        """Calculate vega, the change in an option's price w.r.t. implied volatility"""
+        return self.S*norm.pdf(self.d1)*np.sqrt(self.T)
+
+    def theta(self):
+        """Calculate theta, the change in an option's price w.r.t. time"""
+        if self.option_type == 'call':
+            return (-self.r*self.K*norm.cdf(self.d2)*np.exp(-self.r*self.T) 
+            - (self.sigma*self.S*norm.pdf(self.d1))/(2*np.sqrt(self.T)))
+
+        elif self.option_type == 'put':
+            return (self.r*self.K*norm.cdf(-self.d2)*np.exp(-self.r*self.T) 
+            - (self.sigma*self.S*norm.pdf(self.d1))/(2*np.sqrt(self.T)))
+
+    def rho(self):
+        """Calculate rho, the change in an option's price w.r.t. the interest rate"""
+        if self.option_type == 'call':
+            return self.K*self.T*np.exp(-self.r*self.T)*norm.cdf(self.d2)
+        elif self.option_type == 'put':
+            return -self.K*self.T*np.exp(-self.r*self.T)*norm.cdf(-self.d2)
